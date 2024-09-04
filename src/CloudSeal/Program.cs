195378dcdac77393720ecb5c2626c9ee06c7d2c2
Oleg Shakhmatov <oleg.shakhmatov@billetkontoret.dk>
2024-09-04 12:45:22 +0200

    improve

--- src/CloudSeal/Program.cs
+++ src/CloudSeal/Program.cs
@@ -1,41 +1,54 @@
-﻿using System;
-using System.Diagnostics;
-using System.IO;
+﻿using System.Diagnostics;
 
-if (args.Length < 2)
+Console.Write("Password: ");
+string password = Console.ReadLine();
+
+if (string.IsNullOrEmpty(password))
 {
-    Console.WriteLine("Usage: cloudseal <password> <file_path>");
-    //return;
-}
-
-string password = args[0];
-string filePath = args[1];
-//string password = "1234";
-//string filePath = @"C:\test\test.txt";
-
-if (!File.Exists(filePath))
-{
-    Console.WriteLine("File not found: " + filePath);
+    Console.WriteLine("Password is wrong");
     return;
 }
 
-string outputFilePath = Path.Combine(
-    Path.GetDirectoryName(filePath),
-    Path.GetFileNameWithoutExtension(filePath) + "_sealed.7z"
-);
+string currentDirectory = Directory.GetCurrentDirectory();
+string folderPath = Path.Combine(currentDirectory, "Local");
 
-try
+Directory.CreateDirectory(folderPath);
+
+string sealedFolderPath = Path.Combine(currentDirectory, "SealedCloud");
+Directory.CreateDirectory(sealedFolderPath);
+
+var files = Directory.GetFiles(folderPath, "*.*", SearchOption.AllDirectories);
+foreach (var file in files)
 {
-    ArchiveFile(filePath, outputFilePath, password);
+    if (Path.GetExtension(file) != ".7z" && Path.GetExtension(file) != ".exe")
+    {
+        // Определяем путь файла в структуре SealedCloud
+        string relativePath = Path.GetRelativePath(folderPath, file);
+        string outputFilePath = Path.Combine(
+            sealedFolderPath,
+            Path.GetDirectoryName(relativePath),
+            Path.GetFileNameWithoutExtension(file) + "_sealed.7z"
+        );
+
+        Directory.CreateDirectory(Path.GetDirectoryName(outputFilePath));
+
+        try
+        {
+            ArchiveFile(file, outputFilePath, password);
+            File.Delete(file);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+    }
 }
-catch (Exception ex)
-{
-    Console.WriteLine("An error occurred: " + ex.Message);
-}
+
+DeleteEmptyDirectories(folderPath);
 
 static void ArchiveFile(string inputFile, string outputFile, string password)
 {
-    string command = $".\\7z.exe a -t7z \"{outputFile}\" \"{inputFile}\" -mhe=on -mx=9 -p\"{password}\"";
+    var command = $".\\7z.exe a -t7z \"{outputFile}\" \"{inputFile}\" -mhe=on -mx=9 -p\"{password}\"";
 
     var processStartInfo = new ProcessStartInfo
     {
@@ -48,4 +61,16 @@ static void ArchiveFile(string inputFile, string outputFile, string password)
 
     using var process = Process.Start(processStartInfo);
     process.WaitForExit();
+}
+
+static void DeleteEmptyDirectories(string startLocation)
+{
+    foreach (var directory in Directory.GetDirectories(startLocation))
+    {
+        DeleteEmptyDirectories(directory);
+        if (Directory.GetFiles(directory).Length == 0 && Directory.GetDirectories(directory).Length == 0)
+        {
+            Directory.Delete(directory);
+        }
+    }
 }
